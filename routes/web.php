13c4c7c56d758fc30429b40ff94451a04f964e0e
Danil <martinov2806@gmail.com>
2024-06-03 10:42:58 +0500

    Закомитил

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::resource('users', UsersController::class);
+Route::resource('clients', ClientsController::class);
+Route::resource('Order', OrderController::class);
+Route::resource('Inventory', InventoryController::class);
+Route::resource('Finance', FinanceController::class);
+Route::resource('Mail', MailController::class);
 Route::get('/', function () {
     return view('welcome');
+    
 });
